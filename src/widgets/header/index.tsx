29ee63f00a105661cc4a26ab85009e99fba375c8
KIRA <bfakhruz@gmail.com>
2023-04-16 04:54:18 +0500

    added header widget

---
+++ src/widgets/header/index.tsx
@@ -0,0 +1,10 @@
+import styles from "./styles.module.scss";
+
+export const Header = () => {
+	return (
+		<header class={styles.header}>
+			<p>app name</p>
+			<div>settings</div>
+		</header>
+	)
+}
