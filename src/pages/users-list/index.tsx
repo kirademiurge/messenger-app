cbf7176d832cfb3bb5ace7f87baf38c99d057a7b
KIRA <bfakhruz@gmail.com>
2023-04-16 04:53:34 +0500

    added users list page

---
+++ src/pages/users-list/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../widgets/header";
+import { UsersList } from "../../widgets/users-list";
+
+export default function UsersListPage () {
+	return (
+		<div class="screen">
+			<Header />
+			<UsersList />
+		</div>
+	)
+}
