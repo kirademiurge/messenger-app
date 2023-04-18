abe9a6efbdc90786e415cbbfd4aa0c36d76f4649
KIRA <bfakhruz@gmail.com>
2023-04-18 22:19:01 +0500

    updated ui of users-list widget

--- src/widgets/users-list/ui/users-list-item/index.tsx
+++ src/widgets/users-list/ui/users-list-item/index.tsx
@@ -1,17 +1,20 @@
+import { A } from "@solidjs/router";
 import { User } from "../../../../entities/user";
 import styles from "./styles.module.scss";
 
 export const UsersListItem = (props: User) => {
 	return (
-		<div class={styles.item}>
-			<div>
-				{props.avapic ? <img class={styles.avapic} src={props.avapic} alt="avapic" loading="lazy" /> : <div class={styles.avapic} />}
-			</div>
+		<A href={`user/${props.username}`}>
+			<div class={styles.item}>
+				<div>
+					{props.avapic ? <img class={styles.avapic} src={props.avapic} alt="avapic" loading="lazy" /> : <div class={styles.avapic} />}
+				</div>
 
-			<div class={styles.textBlock}>
-				<p class={styles.name}>{props.name}</p>
-				<p class={styles.lastMessage}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, quam!</p>
+				<div class={styles.textBlock}>
+					<p class={styles.name}>{props.name}</p>
+					<p class={styles.lastMessage}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, quam!</p>
+				</div>
 			</div>
-		</div>
+		</A>
 	)
 }
