cde3c31c946e30ce7dcdfcc25b7d3f55ca12ec39
KIRA <bfakhruz@gmail.com>
2023-04-18 22:18:06 +0500

    updated model of users-list widget

--- src/widgets/users-list/model/index.ts
+++ src/widgets/users-list/model/index.ts
@@ -9,86 +9,17 @@ export const [usersListStore, setUsersListStore] = createStore<UsersListStore>({
 	usersList: [
 		{
 			id: "0",
+			username: "username0",
 			name: "KIRA",
 		},
 		{
 			id: "1",
+			username: "username1",
 			name: "YAGAMI",
 		},
 		{
 			id: "2",
-			name: "LIGHT",
-		},
-		{
-			id: "0",
-			name: "KIRA",
-		},
-		{
-			id: "1",
-			name: "YAGAMI",
-		},
-		{
-			id: "2",
-			name: "LIGHT",
-		},
-		{
-			id: "0",
-			name: "KIRA",
-		},
-		{
-			id: "1",
-			name: "YAGAMI",
-		},
-		{
-			id: "2",
-			name: "LIGHT",
-		},
-		{
-			id: "0",
-			name: "KIRA",
-		},
-		{
-			id: "1",
-			name: "YAGAMI",
-		},
-		{
-			id: "2",
-			name: "LIGHT",
-		},
-		{
-			id: "0",
-			name: "KIRA",
-		},
-		{
-			id: "1",
-			name: "YAGAMI",
-		},
-		{
-			id: "2",
-			name: "LIGHT",
-		},
-		{
-			id: "0",
-			name: "KIRA",
-		},
-		{
-			id: "1",
-			name: "YAGAMI",
-		},
-		{
-			id: "2",
-			name: "LIGHT",
-		},
-		{
-			id: "0",
-			name: "KIRA",
-		},
-		{
-			id: "1",
-			name: "YAGAMI",
-		},
-		{
-			id: "2",
+			username: "username2",
 			name: "LIGHT",
 		},
 	],
