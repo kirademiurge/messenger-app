39e3e5d3411b7a794ae552c037f687de1cfd9b79
KIRA <bfakhruz@gmail.com>
2023-04-18 22:16:59 +0500

    edited header widget

--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -1,12 +1,11 @@
 import { userStore } from "../../entities/user";
-import { config } from "../../shared/config";
 import styles from "./styles.module.scss";
 
 export const Header = () => {
 	return (
 		<header class={styles.header}>
 			{userStore.avapic ? <img class={styles.avapic} src={userStore.avapic} alt="avapic" loading="lazy" /> : <div class={styles.avapic} />}
-			<p class={styles.projectName}>{userStore.name}</p>
+			<p class={styles.projectName}>{userStore.username}</p>
 		</header>
 	)
 }
