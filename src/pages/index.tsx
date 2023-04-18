f94e6b6d95f4f348f7bf2ee92cc33456f80c4425
KIRA <bfakhruz@gmail.com>
2023-04-18 22:15:54 +0500

    updated index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import { lazy } from "solid-js";
 import { Routes, Route, Navigate } from "@solidjs/router";
 
 const MainPage = lazy( () => import("./main") );
+const UserChatPage = lazy( () => import("./user-chat") );
 const SettingsPage = lazy( () => import("./settings") );
 
 export default function PagesWithRouting () {
 	return (
 		<Routes>
 			<Route path="/" component={MainPage} />
+			<Route path="/user/:id" component={UserChatPage} />
 			<Route path="/settings" component={SettingsPage} />
 			<Route path="*">
 				<Navigate href="/" />
