2f96c652f1e5119bc8fe3af44c392147cba262cb
KIRA <bfakhruz@gmail.com>
2023-04-22 20:55:43 +0500

    edited shared textarea

--- src/shared/ui/textarea/index.tsx
+++ src/shared/ui/textarea/index.tsx
@@ -1,17 +1,28 @@
-import { mergeProps } from "solid-js";
+import { createSignal, mergeProps } from "solid-js";
 import styles from "./styles.module.scss";
 
 export interface TextareaProps {
 	placeholder?: string,
 	maxHeight?: string,
+	text?: any,
+	onInput?: any,
+	onChange?: any,
 }
 
 export const Textarea = (props: TextareaProps) => {
 	const merged = mergeProps({maxHeight: "8rem"}, props);
 
-	const onInputHandler: any = (event: any) => {
+	const [text, setText] = createSignal(props.text || "");
+
+	const onChangeHandler = (event: any) => {
+		setText(event.target.value);
+		if (props.onChange) props.onChange(event);
+	}
+	
+	const onInputHandler = (event: any) => {
 		event.target.style.height = "auto";
 		event.target.style.height = `${event.target.scrollHeight}px`;
+		if (props.onInput) props.onInput(event);
 	}
 
 	const style = {
@@ -22,7 +33,9 @@ export const Textarea = (props: TextareaProps) => {
 		<textarea
 			style={style}
 			class={styles.textarea}
+			value={text()}
 			placeholder={props.placeholder}
+			onChange={ onChangeHandler }
 			onInput={ onInputHandler }
 			rows={1}
 		/>
