1af4797d40f6533df3151a86bcc3194a3e9d241f
KIRA <bfakhruz@gmail.com>
2023-04-22 19:08:17 +0500

    edited types of user entity

--- src/entities/user/api/types/index.ts
+++ src/entities/user/api/types/index.ts
@@ -1,7 +1,10 @@
+import { avapicColor } from "../../../../shared/api";
+
 export interface User {
 	id: string,
 	username: string,
 	name: string,
 	avapic?: string,
 	bio?: string,
+	color: avapicColor,
 }
