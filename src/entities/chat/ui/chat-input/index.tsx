18911a00af667087c0355c12d6cb0989b1efe88e
KIRA <bfakhruz@gmail.com>
2023-04-22 20:54:17 +0500

    edited ui of chat entity

--- src/entities/chat/ui/chat-input/index.tsx
+++ src/entities/chat/ui/chat-input/index.tsx
@@ -1,12 +1,25 @@
+import { createEffect, createSignal } from "solid-js";
 import { Textarea } from "../../../../shared/ui";
 import styles from "./styles.module.scss";
 import { IoSend } from "solid-icons/io";
 
 export const ChatInput = () => {
+	const [text, setText] = createSignal("");
+
+	const onChangeHandler = (event: any) => {
+		setText(event.target.value);
+		event.target.value = "";
+	}
+	
+	const handleSubmit = (event: any) => {
+		event.preventDefault();
+		setText("");
+	}
+
 	return (
-		<div class={styles.chatInput}>
-			<Textarea placeholder="Type a message..." />
-			<IoSend size={24} class={styles.sendButton} />
-		</div>
+		<form onSubmit={handleSubmit} class={styles.chatInput}>
+			<Textarea text={text()} onChange={onChangeHandler} placeholder="Type a message..." />
+			<button type="submit" class={styles.sendButton}><IoSend size={24} /></button>
+		</form>
 	)
 }
